15334a610e4a62479b25d812fab4ec712bc2c514
Dustin Moore <dmoore@frostbytecafe.com>
2017-02-23 10:34:08 -0700

    HTTP - Last step

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,20 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { NgModule }             from '@angular/core';
+import { BrowserModule }        from '@angular/platform-browser';
+import { FormsModule }          from '@angular/forms';
 
-import { AppComponent }  from './app.component';
-import { HeroDetailComponent } from './hero-detail.component'
-import { HeroesComponent} from './heroes.component';
-import { DashboardComponent } from './dashboard.component'
-import { HeroService } from './hero.service';
+import { AppComponent }         from './app.component';
+import { HeroDetailComponent }  from './hero-detail.component'
+import { HeroesComponent}       from './heroes.component';
+import { DashboardComponent }   from './dashboard.component'
+import { HeroService }          from './hero.service';
+
+import { AppRoutingModule }     from './app-routing.module';
 
 @NgModule({
   imports:      [ 
                   BrowserModule,
                   FormsModule,
-                  RouterModule.forRoot([
-                    {
-                      path: 'heroes',
-                      component: HeroesComponent
-                    },
-                    {
-                      path: 'dashboard',
-                      component: DashboardComponent
-                    },
-                    {
-                      path: 'detail/:id',
-                      redirectTo: '/dashboard',
-                      pathMatch: 'full'
-                    },
-                    {
-                      path: '',
-                      component: HeroDetailComponent
-                    }
-                  ])
+                  AppRoutingModule
                 ],
   declarations: [ 
                   AppComponent,
